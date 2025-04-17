dd703138bf44fc2a4b4e73f8db5590eee67b0cf9
srikanth-io <info.srikanthsankar@gmail.com>
2025-04-17 23:18:54 +0530

    init: modules and components added also tailwind css added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: "full"
+},
+{
+    path: 'dashboard',
+    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
+}
+];
